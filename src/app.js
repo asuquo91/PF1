a5d0082cc010d6fa77b664a6d7e12694e6aaf967
asuquo91 <136271445+asuquo91@users.noreply.github.com>
2023-09-04 11:02:44 +0100

    changed amount of stars on mobiles

--- src/app.js
+++ src/app.js
@@ -25,15 +25,15 @@ hamburgerButton.addEventListener('click', toggleButton)
 // Function to generate a random number within a specified range
 function randomInRange(min, max) {
     return Math.random() * (max - min) + min;
-}
-
-// Function to create a new star element with randomized animation properties
-function createStar() {
+  }
+  
+  // Function to create a new star element with randomized animation properties
+  function createStar() {
     const star = document.createElement("div");
     const size = Math.random() * 2; // Adjust the size range of stars
     const duration = Math.random() * 80 + 5; // Adjust the duration of the animation
     const initialOpacity = Math.random(); // Random initial opacity
-
+  
     star.style.width = size + "px";
     star.style.height = size + "px";
     star.style.opacity = initialOpacity; // Set the initial opacity
@@ -42,22 +42,28 @@ function createStar() {
     star.style.top = `${randomInRange(0, 50)}%`; // Start some stars halfway down
     star.style.animationDelay = Math.random() * 5 + "s";
     star.style.animationDuration = duration + "s";
-
+  
     return star;
-}
-
-// Function to initialize the falling stars
-function initStars() {
+  }
+  
+  // Function to initialize the falling stars
+  function initStars() {
     const starsContainer = document.querySelector(".stars");
-
+  
     // Number of stars you want
-    const numberOfStars = 1000;
-
-    for (let i = 0; i < numberOfStars; i++) {
-        const star = createStar();
-        starsContainer.appendChild(star);
+    let numberOfStars = 1000; // Default number of stars
+  
+    // Check screen width and adjust numberOfStars if it's under 700px
+    if (window.innerWidth < 700) {
+      numberOfStars = 500; // Adjust the number of stars for smaller screens
     }
-}
-
-// Call initStars to generate stars when the script is loaded
-initStars();
+  
+    for (let i = 0; i < numberOfStars; i++) {
+      const star = createStar();
+      starsContainer.appendChild(star);
+    }
+  }
+  
+  // Call initStars to generate stars when the script is loaded
+  initStars();
+  
